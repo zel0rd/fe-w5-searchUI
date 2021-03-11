f961b17b8436b0caa59212825681e34f29bd63be
younggil <zelord.kwoun@gmail.com>
2021-03-11 10:49:56 +0900

    update recentkeyword when item is null

--- public/js/recentKeyWord.js
+++ public/js/recentKeyWord.js
@@ -1,15 +1,19 @@
 import { _ } from './util.js';
 
 function setLocalStorage(){
-    let arr = localStorage.getItem('key').split(",");
-    const inputBox = _.$(".input_box")
-    inputBox.addEventListener("keyup", function(e){
-        if(e.key === "Enter"){
-            arr.push(e.target.value)
-            console.log(arr)
-            localStorage.setItem("key", arr);
-        }
-    })
+    if(localStorage.getItem('key')){
+        let arr = localStorage.getItem('key').split(",");
+        const inputBox = _.$(".input_box")
+        inputBox.addEventListener("keyup", function(e){
+            if(e.key === "Enter"){
+                arr.push(e.target.value)
+                console.log(arr)
+                localStorage.setItem("key", arr);
+            }
+        })
+    } else {
+        localStorage.setItem("key","init")
+    }
 }
 
 export {  setLocalStorage }
